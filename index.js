11ad563995de207ee872a6b2118b9d71fb3301c9
generous-calculator-4715 <andrew.lewell@gmail.com>
2019-09-23 11:02:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var theBeatlesPlay = [];
+  
+  for (var i = 0; i < 4; ++i) {
+    theBeatlesPlay.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return theBeatlesPlay;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var johnLennonFacts = [];
+  
+  while (facts[i] !== null) {
+    johnLennonFacts.push[facts[i] + "!!!"];
+    ++i;
+  }
+  return johnLennonFacts;
+}
